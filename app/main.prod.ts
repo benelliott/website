99b9dc7bdee49436a1d1eb9ff334598a946d50ec
Ben Elliott <bengelliott@gmail.com>
2016-11-10 00:08:48 +0000

    enableProdMode on prod build, use compressed assets

--- app/main.prod.ts
+++ app/main.prod.ts
@@ -1,4 +1,6 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowser } from '@angular/platform-browser';
 import { BGEModuleNgFactory } from './aot/app/app.module.ngfactory';
 
+enableProdMode();
 platformBrowser().bootstrapModuleFactory(BGEModuleNgFactory);
